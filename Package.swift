17c4cf630b094cffeaacb128b69cdc1488ed2f64
Victor C Tavernari <victortavernari@gmail.com>
2024-09-03 05:40:45 +0100

    Rollback swift-tool to 5.10 and swift-testing to 0.9.0

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
             // Adding the Swift Testing library as a dependency
-            .package(url: "https://github.com/swiftlang/swift-testing.git", from: "0.12.0")
+            .package(url: "https://github.com/swiftlang/swift-testing.git", from: "0.9.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
