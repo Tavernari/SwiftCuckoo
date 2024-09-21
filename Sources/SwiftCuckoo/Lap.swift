9f943fb5216502c03d85e0e608028ee7ff857539
Dante Hargrow <dante.hargrow@vml.com>
2024-09-21 10:34:56 -0500

    Adding Lap status enum and refactoring error names

--- Sources/SwiftCuckoo/Lap.swift
+++ Sources/SwiftCuckoo/Lap.swift
@@ -6,6 +6,12 @@ import Foundation
 /// of a single lap, providing a way to monitor intervals within a task or session.
 public struct Lap: Equatable, Hashable {
 
+    /// Defines the different states of a  `Lap`.
+    public enum LapStatus: Equatable {
+        case active                /// Indicates the lap is active.
+        case inactive              /// Indicates the lap is inactive.
+    }
+
     /// A unique identifier for the lap instance.
     public var id: Identifier
 
@@ -26,13 +32,20 @@ public struct Lap: Equatable, Hashable {
         self.endTime = endTime
     }
 
+    public mutating func start(on date: Date = Date()) throws {
+        guard lapStatus() == .inactive else {
+            throw Error.lapActive
+        }
+        self.startTime = date
+    }
+
     /// Stops the lap at the specified date.
     ///
     /// - Parameter date: The date when the lap ends. Defaults to the current date.
     /// - Throws: An error if the lap has already ended.
     public mutating func stop(on date: Date = Date()) throws {
-        guard self.endTime == nil else {
-            throw Error.lapAlreadyStopped
+        guard lapStatus() == .active else {
+            throw Error.lapNotActive
         }
 
         self.endTime = date
@@ -44,17 +57,22 @@ public struct Lap: Equatable, Hashable {
     /// - Returns: The duration of the lap as a `TimeInterval`.
     public func duration() throws -> TimeInterval {
         guard let endTime else {
-            throw Error.lapNotEnded
+            throw Error.lapActive
         }
 
         return endTime.timeIntervalSince(startTime)
     }
-
-    /// Determines the current status of the lap.
+    
+    /// Determines the current status of a lap.
     ///
-    /// - Returns: A boolean indicating whether the lap is still running or has completed.
-    public func isRunning() -> Bool {
-        return self.endTime == nil
+    /// - Returns: The current status as a `LapStatus` enumerator indicating whether
+    ///   the session is active, or inactive.
+    public func lapStatus() -> LapStatus {
+        guard let endTime else {
+            return .active
+        }
+
+        return .inactive
     }
 }
 
@@ -81,7 +99,7 @@ extension Lap {
 extension Lap {
     /// Error types specific to the `Lap` structure, used for error handling.
     public enum Error: CustomNSError {
-        case lapAlreadyStopped      /// Indicates that the lap has already been stopped.
-        case lapNotEnded            /// Indicates that the lap has not ended.
+        case lapNotActive      /// Indicates that the lap is inactive.
+        case lapActive         /// Indicates that the lap is already active.
     }
 }
